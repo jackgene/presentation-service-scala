b219c543e3c7c2bfc3496e78bf76330e17d6dfa5
Jack Leow <jackgene@gmail.com>
2022-10-16 09:55:25 -0700

    Added arbitrary word token extraction function.

--- app/model/Token.scala
+++ app/model/Token.scala
@@ -1,6 +1,120 @@
 package model
 
+import scala.util.matching.Regex
+
 object Token {
+  private val ValidWordPattern: Regex = """([A-Za-z]{3,}(?:-[A-Za-z]{3,})*)""".r
+  private val StopWords: Set[String] = Set(
+    "about",
+    "above",
+    "after",
+    "again",
+    "against",
+    "all",
+    "and",
+    "any",
+    "are",
+    "because",
+    "been",
+    "before",
+    "being",
+    "below",
+    "between",
+    "both",
+    "but",
+    "can",
+    "did",
+    "does",
+    "doing",
+    "down",
+    "during",
+    "each",
+    "few",
+    "for",
+    "from",
+    "further",
+    "had",
+    "has",
+    "have",
+    "having",
+    "her",
+    "here",
+    "hers",
+    "herself",
+    "him",
+    "himself",
+    "his",
+    "how",
+    "into",
+    "its",
+    "itself",
+    "just",
+    "me",
+    "more",
+    "most",
+    "myself",
+    "nor",
+    "not",
+    "now",
+    "off",
+    "once",
+    "only",
+    "other",
+    "our",
+    "ours",
+    "ourselves",
+    "out",
+    "over",
+    "own",
+    "same",
+    "she",
+    "should",
+    "some",
+    "such",
+    "than",
+    "that",
+    "the",
+    "their",
+    "theirs",
+    "them",
+    "themselves",
+    "then",
+    "there",
+    "these",
+    "they",
+    "this",
+    "those",
+    "through",
+    "too",
+    "under",
+    "until",
+    "very",
+    "was",
+    "were",
+    "what",
+    "when",
+    "where",
+    "which",
+    "while",
+    "who",
+    "whom",
+    "why",
+    "will",
+    "with",
+    "you",
+    "your",
+    "yours",
+    "yourself",
+    "yourselves",
+  )
+
+  def words(text: String): Seq[String] = text.trim.
+    split("""[^\w\-]""").toSeq.
+    map { _.toLowerCase }.
+    collect {
+      case ValidWordPattern(word: String) if !StopWords.contains(word) => word
+    }
+
   private val TokensByString: Map[String,String] = Map(
     // C/C++
     "c" -> "C",
@@ -54,19 +168,15 @@ object Token {
     "typescript" -> "TypeScript",
   )
 
-  def languageFromFirstWord(text: String): Seq[String] = {
-    val normalizedFirstWordOpt: Option[String] = text.trim.
-      split("""[^\w#]""").headOption.
-      map { _.toLowerCase }
-    normalizedFirstWordOpt.flatMap { TokensByString.get }.toSeq
-  }
+  def languageFromFirstWord(text: String): Seq[String] = text.trim.
+    split("""[^\w#]""").headOption.
+    map { _.toLowerCase }.
+    flatMap(TokensByString.get).toSeq
 
-  def languagesFromWords(text: String): Seq[String] = {
-    val normalizedWords: Seq[String] = text.trim.
-      split("""[^\w#]""").toSeq.
-      map { _.toLowerCase }
-    normalizedWords.flatMap { TokensByString.get }
-  }
+  def languagesFromWords(text: String): Seq[String] = text.trim.
+    split("""[^\w#]""").toSeq.
+    map { _.toLowerCase }.
+    flatMap(TokensByString.get)
 
   private val TokensByLetter: Map[Char,String] = Map(
     'e' -> "Python",
