bd4b3619a5f6ac7ca5335cfdee4ca6fa2791e70c
Jack Leow <jackgene@gmail.com>
2022-12-25 12:49:58 -0800

    Genericized Frequencies and added unit tests.

---
+++ test/actors/FrequenciesSpec.scala
@@ -0,0 +1,87 @@
+package actors
+
+import org.scalatestplus.play.PlaySpec
+
+class FrequenciesSpec extends PlaySpec {
+  "Frequencies" must {
+    val empty = Frequencies[String]()
+
+    "be initially empty" in {
+      // Set up & Test
+      val instance = empty
+
+      // Verify
+      assert(instance.countsByItem.isEmpty)
+      assert(instance.itemsByCount.isEmpty)
+    }
+
+    "record correct counts" in {
+      // Set up & Test
+      val instance = empty.
+        updated("test-1", -1). // test-1: 0
+        updated("test-1", 2).  // test-1: 2
+        updated("test-1", 3).  // test-1: 5
+        updated("test-1", -4). // test-1: 1
+        updated("test-2", 5)   // test-2: 5
+
+      // Verify
+      assert(instance.countsByItem == Map("test-1" -> 1, "test-2" -> 5))
+      assert(instance.itemsByCount == Map(1 -> Seq("test-1"), 5 -> Seq("test-2")))
+    }
+
+    "append item to itemsByCount when incremented" in {
+      // Set up & Test
+      val instance = empty.
+        updated("test-1", 1).
+        updated("test-2", 1) // Incrementing from 0 -> 1
+
+      // Verify
+      // Incremented value should be appended
+      assert(instance.itemsByCount == Map(1 -> Seq("test-1", "test-2")))
+    }
+
+    "prepend item to itemsByCount when decremented" in {
+      // Set up & Test
+      val instance = empty.
+        updated("test-1", 1).
+        updated("test-2", 2).
+        updated("test-2", -1) // Decrement from 2 -> 1
+
+      // Verify
+      // Decremented value should be prepended
+      assert(instance.itemsByCount == Map(1 -> Seq("test-2", "test-1")))
+    }
+
+    "no-op when updated with a 0 delta" in {
+      // Set up
+      val instance = empty.updated("test", 42)
+
+      // Test
+      val updated = instance.updated("test", 0)
+
+      // Verify
+      assert(instance == updated)
+      assert(instance eq updated)
+    }
+
+    "omit zero counts" in {
+      // Set up & Test
+      val instance = empty.
+        updated("test", 1).
+        updated("test", -1)
+
+      // Verify
+      assert(instance.countsByItem.isEmpty)
+      assert(instance.itemsByCount.isEmpty)
+    }
+
+    "omit negative counts" in {
+      // Set up & Test
+      val instance = empty.updated("test", -1)
+
+      // Verify
+      assert(instance.countsByItem.isEmpty)
+      assert(instance.itemsByCount.isEmpty)
+    }
+  }
+}
