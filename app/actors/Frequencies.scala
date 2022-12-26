04e28f21ef11105ad25647d4016a7b522866f88c
Jack Leow <jackgene@gmail.com>
2022-12-26 06:36:05 -0800

    Doc comment for Frequencies.

--- app/actors/Frequencies.scala
+++ app/actors/Frequencies.scala
@@ -1,9 +1,25 @@
 package actors
 
+/**
+ * Immutable data structure that tracks frequencies/counts of anything.
+ *
+ * @param countsByItem mapping of item -> count
+ * @param itemsByCount mapping of count -> items
+ * @tparam T type of item being counted
+ */
 case class Frequencies[T](
   countsByItem: Map[T, Int] = Map[T, Int](),
   itemsByCount: Map[Int, Seq[T]] = Map[Int, Seq[T]]()
 ) {
+  /**
+   * Updates the counts of a single item by incrementing
+   * (positive-delta) or decrementing (negative-delta) by the given
+   * value.
+   *
+   * @param item the item whose count is to be updated
+   * @param delta the amount to change the count
+   * @return updated copy
+   */
   def updated(item: T, delta: Int): Frequencies[T] = {
     if (delta == 0) this
     else {
