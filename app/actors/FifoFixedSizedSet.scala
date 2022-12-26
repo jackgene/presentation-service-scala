21cf71cb41625124d502660c10777773c7fffbaf
Jack Leow <jackgene@gmail.com>
2022-12-26 07:01:48 -0800

    Renamed FifoFixedSizeSet to FifoFixedSizedSet. Added doc comments and unit tests.

---
+++ app/actors/FifoFixedSizedSet.scala
@@ -0,0 +1,118 @@
+package actors
+
+import play.api.libs.json._
+
+object FifoFixedSizedSet {
+  /**
+   * The effect of adding a value to the set:
+   */
+  sealed trait Effect[T]
+  /**
+   * Indicates that an item was added to the set without evicting
+   * anything from the set.
+   *
+   * This happens when adding a new item to a set that is not full.
+   */
+  case class Added[T]() extends Effect[T]
+  /**
+   * Indicates that an item was added to the set evicting an existing
+   * item from the set.
+   *
+   * This happens when adding a new item to a set that is full.
+   */
+  case class AddedEvicting[T](value: T) extends Effect[T]
+  /**
+   * Indicates that an item was not added to the set.
+   *
+   * Note that while the contents of the set would remaing the same,
+   * the insertion order may change.
+   *
+   * This happens when adding an existing item to a set.
+   */
+  case class NotAdded[T]() extends Effect[T]
+
+  def apply[T](maxSize: Int): FifoFixedSizedSet[T] = new FifoFixedSizedSet[T](maxSize)
+
+  implicit def writes[T](implicit itemWrites: Writes[T]): Writes[FifoFixedSizedSet[T]] =
+    (set: FifoFixedSizedSet[T]) => Json.arr(set.toSeq.map(itemWrites.writes))
+}
+
+/**
+ * First-In, First-Out fixed sized set.
+ *
+ * This is basically an LRU-cache that returns evictions as items are
+ * added.
+ *
+ * @tparam T the element type
+ */
+class FifoFixedSizedSet[T] private(
+  maxSize: Int,
+  uniques: Set[T] = Set[T](),
+  insertionOrder: IndexedSeq[T] = Vector[T]()
+) {
+  import FifoFixedSizedSet._
+
+  private def copy(
+      uniques: Set[T] = uniques, insertionOrder: IndexedSeq[T]):
+      FifoFixedSizedSet[T] =
+    new FifoFixedSizedSet(maxSize, uniques, insertionOrder)
+
+  /**
+   * Adds a single item to this set, returning its effect.
+   *
+   * See [[Effect]] for details.
+   *
+   * @param item item to add
+   * @return updated copy of this set, and effects of the addition
+   */
+  def add(item: T): (FifoFixedSizedSet[T], Effect[T]) =
+    if (uniques.contains(item))
+      if (item == insertionOrder.last) (this, NotAdded())
+      else
+        (
+          copy(insertionOrder = insertionOrder.filterNot(_ == item) :+ item),
+          NotAdded()
+        )
+    else
+      if (uniques.size < maxSize)
+        (
+          copy(
+            uniques = uniques + item,
+            insertionOrder = insertionOrder :+ item
+          ),
+          Added()
+        )
+      else
+        (
+          copy(
+            uniques = uniques + item - insertionOrder.head,
+            insertionOrder = insertionOrder.tail :+ item
+          ),
+          AddedEvicting(insertionOrder.head)
+        )
+
+  /**
+   * Adds the items to this set, returning the effect of each addition.
+   *
+   * See [[Effect]] for details.
+   *
+   * @param items items to add
+   * @return updated copy of this set, and effects of each addition
+   */
+  def addAll(items: Seq[T]): (FifoFixedSizedSet[T], Seq[Effect[T]]) =
+    items.foldLeft((this, IndexedSeq[Effect[T]]())) {
+      (
+        accum: (FifoFixedSizedSet[T], IndexedSeq[Effect[T]]), item: T
+      ) =>
+
+      val (accumSet: FifoFixedSizedSet[T], accumUpdates: IndexedSeq[Effect[T]]) = accum
+      val (nextAccumSet: FifoFixedSizedSet[T], update: Effect[T]) = accumSet.add(item)
+
+      (nextAccumSet, accumUpdates :+ update)
+    }
+
+  def toSeq: Seq[T] = insertionOrder
+
+  override def toString: String =
+    insertionOrder.mkString("FifoFixedSizedSet(", ", ", ")")
+}
---
+++ test/actors/FifoFixedSizedSetSpec.scala
@@ -0,0 +1,170 @@
+package actors
+
+import org.scalatestplus.play.PlaySpec
+
+class FifoFixedSizedSetSpec extends PlaySpec {
+  "A FifoFixedSizeSet of size 2" when {
+    val empty: FifoFixedSizedSet[String] = FifoFixedSizedSet[String](2)
+
+    "no items are added" must {
+      // Set up
+      val instance: FifoFixedSizedSet[String] = empty
+
+      "be empty" in {
+        // Test & Verify
+        assert(instance.toSeq.isEmpty)
+      }
+
+      "accept 1 new item without evicting" in {
+        // Set up & Test
+        val (actualUpdatedInstance, actualEffect) = instance.add("test")
+
+        // Verify
+        assert(actualEffect == FifoFixedSizedSet.Added())
+        assert(actualUpdatedInstance.toSeq == Seq("test"))
+      }
+
+      "accept 2 new items without evicting" in {
+        // Set up & Test
+        val (actualUpdatedInstance, actualEffects) =
+          instance.addAll(Seq("test-1", "test-2"))
+
+        // Verify
+        assert(actualEffects == Seq(FifoFixedSizedSet.Added(), FifoFixedSizedSet.Added()))
+        assert(actualUpdatedInstance.toSeq == Seq("test-1", "test-2"))
+      }
+
+      "accept 3 new items evicting the first" in {
+        // Set up & Test
+        val (actualUpdatedInstance, actualEffects) =
+          instance.addAll(Seq("test-1", "test-2", "test-3"))
+
+        // Verify
+        assert(
+          actualEffects == Seq(
+            FifoFixedSizedSet.Added(),
+            FifoFixedSizedSet.Added(),
+            FifoFixedSizedSet.AddedEvicting("test-1")
+          )
+        )
+        assert(actualUpdatedInstance.toSeq == Seq("test-2", "test-3"))
+      }
+    }
+
+    "1 item has been added" must {
+      // Set up
+      val (instance: FifoFixedSizedSet[String], _) = empty.add("test-1")
+
+      "contain the item" in {
+        // Test & Verify
+        assert(instance.toSeq == Seq("test-1"))
+      }
+
+      "accept 1 new item without evicting" in {
+        // Set up & Test
+        val (actualUpdatedInstance, actualEffect) = instance.add("test-2")
+
+        // Verify
+        assert(actualEffect == FifoFixedSizedSet.Added())
+        assert(actualUpdatedInstance.toSeq == Seq("test-1", "test-2"))
+      }
+
+      "not accept the existing item again" in {
+        // Set up & Test
+        val (actualUpdatedInstance, actualEffect) = instance.add("test-1")
+
+        // Verify
+        assert(actualEffect == FifoFixedSizedSet.NotAdded())
+        assert(actualUpdatedInstance.toSeq == Seq("test-1"))
+      }
+
+      "accept 2 new items evicting the existing item" in {
+        // Set up & Test
+        val (actualUpdatedInstance, actualEffects) =
+          instance.addAll(Seq("test-2", "test-3"))
+
+        // Verify
+        assert(
+          actualEffects == Seq(
+            FifoFixedSizedSet.Added(),
+            FifoFixedSizedSet.AddedEvicting("test-1")
+          )
+        )
+        assert(actualUpdatedInstance.toSeq == Seq("test-2", "test-3"))
+      }
+    }
+
+    "2 items has been added" must {
+      // Set up
+      val (instance: FifoFixedSizedSet[String], _) = empty.addAll(Seq("test-1", "test-2"))
+
+      "contain the items" in {
+        // Test & Verify
+        assert(instance.toSeq == Seq("test-1", "test-2"))
+      }
+
+      "accept 1 new item evicting the first existing item" in {
+        // Set up & Test
+        val (actualUpdatedInstance, actualEffect) = instance.add("test-3")
+
+        // Verify
+        assert(actualEffect == FifoFixedSizedSet.AddedEvicting("test-1"))
+        assert(actualUpdatedInstance.toSeq == Seq("test-2", "test-3"))
+      }
+
+      "not accept an existing item again, but update its insertion order" in {
+        // Set up & Test
+        val (actualUpdatedInstance, actualEffect) = instance.add("test-1")
+
+        // Verify
+        assert(actualEffect == FifoFixedSizedSet.NotAdded())
+        assert(actualUpdatedInstance.toSeq == Seq("test-2", "test-1"))
+      }
+
+      "accept 2 new items evicting all existing items" in {
+        // Set up & Test
+        val (actualUpdatedInstance, actualEffects) =
+          instance.addAll(Seq("test-3", "test-4"))
+
+        // Verify
+        assert(
+          actualEffects == Seq(
+            FifoFixedSizedSet.AddedEvicting("test-1"),
+            FifoFixedSizedSet.AddedEvicting("test-2")
+          )
+        )
+        assert(actualUpdatedInstance.toSeq == Seq("test-3", "test-4"))
+      }
+
+      "not accept 2 existing items, but update their insertion order" in {
+        // Set up & Test
+        val (actualUpdatedInstance, actualEffects) =
+          instance.addAll(Seq("test-2", "test-1"))
+
+        // Verify
+        assert(
+          actualEffects == Seq(
+            FifoFixedSizedSet.NotAdded(),
+            FifoFixedSizedSet.NotAdded()
+          )
+        )
+        assert(actualUpdatedInstance.toSeq == Seq("test-2", "test-1"))
+      }
+
+      "accept a new item, but not an existing item, updating the insertion order of the existing item" in {
+        // Set up & Test
+        val (actualUpdatedInstance, actualEffects) =
+          instance.addAll(Seq("test-1", "test-3"))
+
+        // Verify
+        assert(
+          actualEffects == Seq(
+            FifoFixedSizedSet.NotAdded(),             // But test-1 should no longer be "first in"
+            FifoFixedSizedSet.AddedEvicting("test-2") // test-2 is evicted
+          )
+        )
+        assert(actualUpdatedInstance.toSeq == Seq("test-1", "test-3"))
+      }
+    }
+  }
+}
