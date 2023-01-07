c7e145fd338cb538471185925c639af0697a8b69
Jack Leow <jackgene@gmail.com>
2023-01-07 12:28:48 -0800

    Set sizeRange to 100.

--- test/common/CommonProp.scala
+++ test/common/CommonProp.scala
@@ -7,7 +7,7 @@ import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 abstract class CommonProp extends AnyPropSpec with ScalaCheckPropertyChecks {
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration = PropertyCheckConfiguration(
     minSuccessful = PosInt.ensuringValid(Integer.getInteger("test.prop.minSuccessful", 1_000)),
-    sizeRange = PosInt.ensuringValid(Integer.getInteger("test.prop.sizeRange", 1_000)),
+    sizeRange = PosInt.ensuringValid(Integer.getInteger("test.prop.sizeRange", 100)),
     workers = PosInt.ensuringValid(math.max(2, Runtime.getRuntime.availableProcessors * 4 / 3))
   )
 }
