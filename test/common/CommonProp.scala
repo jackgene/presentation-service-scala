74ec37fd56e44ea518b30853fa8ca49f58a9ba78
Jack Leow <jackgene@gmail.com>
2023-01-06 18:45:03 -0800

    Set ScalaCheck minimum workers to 3.

--- test/common/CommonProp.scala
+++ test/common/CommonProp.scala
@@ -8,6 +8,6 @@ abstract class CommonProp extends AnyPropSpec with ScalaCheckPropertyChecks {
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration = PropertyCheckConfiguration(
     minSuccessful = PosInt.ensuringValid(Integer.getInteger("test.prop.minSuccessful", 1_000)),
     sizeRange = PosInt.ensuringValid(Integer.getInteger("test.prop.sizeRange", 1_000)),
-    workers = PosInt.ensuringValid(math.max(2, Runtime.getRuntime.availableProcessors * 3 / 4))
+    workers = PosInt.ensuringValid(math.max(3, Runtime.getRuntime.availableProcessors * 3 / 4))
   )
 }
