ba10078788852181bfabb975e42aeefaeb9be70e
Jack Leow <jackgene@gmail.com>
2023-01-08 14:23:14 -0800

    Added validation in MappedKeywordsTokenizer to reject invalid raw tokens.

--- app/actors/tokenizing/MappedKeywordsTokenizer.scala
+++ app/actors/tokenizing/MappedKeywordsTokenizer.scala
@@ -1,22 +1,38 @@
 package actors.tokenizing
 
+import scala.util.matching.Regex
+
+object MappedKeywordsTokenizer {
+  private val WordSeparatorPattern: Regex = """[\s,./|?!]""".r
+}
+
 class MappedKeywordsTokenizer private[tokenizing](
-  keywordsByRawToken: Map[String, String]
-) extends Tokenizer {
+                                                   keywordsByRawToken: Map[String, String]
+                                                 ) extends Tokenizer {
+
+  import MappedKeywordsTokenizer.*
+
   if (keywordsByRawToken.isEmpty) {
     throw new IllegalArgumentException("keywordsByRawToken must not be empty")
   }
+  {
+    val invalidRawTokens: Set[String] = keywordsByRawToken.keySet.
+      filter {
+        WordSeparatorPattern.findFirstIn(_).nonEmpty
+      }
+    if (invalidRawTokens.nonEmpty) {
+      throw new IllegalArgumentException(
+        s"some keyword mappings have invalid raw tokens: ${invalidRawTokens.mkString("{", ",", "}")}"
+      )
+    }
+  }
+
   private val keywordsByLowerCasedToken: Map[String, String] = keywordsByRawToken.
     map { case (keyword: String, token: String) =>
       keyword.toLowerCase -> token
     }
-  private val keywordAlphabet: String = keywordsByLowerCasedToken.keySet.
-    flatMap(_.toCharArray).
-    mkString
-  private val splitPattern: String = s"[^${keywordAlphabet}]+"
 
-  override def apply(text: String): Seq[String] = text.trim.
-    toLowerCase.
-    split(splitPattern).to(LazyList).
+  override def apply(text: String): Seq[String] = WordSeparatorPattern.
+    split(text.trim.toLowerCase).to(LazyList).
     flatMap(keywordsByLowerCasedToken.get)
 }
