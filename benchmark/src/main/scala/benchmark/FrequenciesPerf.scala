cb8b87eb28c0b06ac29dcec2015676f2c6ad63ed
Jack Leow <jackgene@gmail.com>
2023-01-15 08:45:03 -0800

    Added JMH benchmark for Frequencies.

---
+++ benchmark/src/main/scala/benchmark/FrequenciesPerf.scala
@@ -0,0 +1,65 @@
+package benchmark
+
+import actors.counter.Frequencies
+import org.openjdk.jmh.annotations.*
+
+import java.util.concurrent.TimeUnit
+
+/**
+ * Benchmark comparing implementations using [[Frequencies]] against
+ * a baseline of naïve implementations using only Scala collections.
+ */
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+class FrequenciesPerf:
+  private val existingVotes: Map[String, String] = Map(
+    "Alice" -> "Java", "Bob" -> "Scala"
+  )
+  private val existingFrequencies: Frequencies[String] = Frequencies().
+    updated("Java", 1).
+    updated("Scala", 1)
+
+  @Benchmark
+  def newVoteBaseline(): Map[Int, Iterable[String]] =
+    val updatedVotes: Map[String, String] =
+      existingVotes.updated("Charlie", "Scala")
+    updatedVotes.
+      // Counts by item
+      groupMap(_._2)(_._1).map { case (key, value) => key -> value.size }.
+      // Items by count - note no prepend/append rule considered
+      groupMap(_._2)(_._1)
+
+  @Benchmark
+  def newVoteUsingFrequencies(): Map[Int, Iterable[String]] =
+    val updatedVotes: Map[String, String] =
+      existingVotes.updated("Charlie", "Scala")
+    val removed: Option[String] = existingVotes.get("Charlie")
+    val updatedFrequencies: Frequencies[String] = removed match
+      case Some(_: String) => throw new IllegalStateException() 
+      case None => existingFrequencies.increment(existingVotes("Charlie"))
+
+    updatedFrequencies.itemsByCount
+
+  @Benchmark
+  def changedVoteBaseline(): Map[Int, Iterable[String]] =
+    val updatedVotes: Map[String, String] =
+      existingVotes.updated("Alice", "Elm")
+    updatedVotes.
+      // Counts by item
+      groupMap(_._2)(_._1).map { case (key, value) => key -> value.size }.
+      // Items by count - note no prepend/append rule considered
+      groupMap(_._2)(_._1)
+
+  @Benchmark
+  def changedVoteUsingFrequencies(): Map[Int, Iterable[String]] =
+    val updatedVotes: Map[String, String] =
+      existingVotes.updated("Alice", "Elm")
+    val removed: Option[String] = existingVotes.get("Alice")
+    val updatedFrequencies: Frequencies[String] = removed match
+      case Some(item: String) => existingFrequencies.
+        increment(updatedVotes("Alice")).
+        decrement(item)
+      case None => throw new IllegalStateException("removal expected")
+
+    updatedFrequencies.itemsByCount
--- build.sbt
+++ build.sbt
@@ -6,3 +6,6 @@ lazy val commonSettings = Seq(
 lazy val `service-play` = (project in file("service-play")).
   settings(commonSettings).
   enablePlugins(PlayScala)
+lazy val benchmark = (project in file("benchmark")).
+  settings(commonSettings).
+  dependsOn(`service-play`)
