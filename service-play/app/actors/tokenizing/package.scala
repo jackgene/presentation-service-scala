0ec05d72c59116a4aed12c5b4743004526b7a31d
Jack Leow <jleow@tubi.tv>
2023-08-26 15:10:53 -0700

    Made tokenizer creation more idiomatic Scala.

--- service-play/app/actors/tokenizing/package.scala
+++ service-play/app/actors/tokenizing/package.scala
@@ -2,12 +2,4 @@ package actors
 
 package object tokenizing {
   type Tokenizer = String => Seq[String]
-
-  def mappedKeywordsTokenizer(keywordsByRawToken: Map[String, String]): Tokenizer =
-    if (keywordsByRawToken.isEmpty) NoOpTokenizer
-    else new MappedKeywordsTokenizer(keywordsByRawToken)
-
-  def normalizedWordsTokenizer(
-    stopWords: Set[String] = Set(), minWordLength: Int, maxWordLength: Int
-  ): Tokenizer = new NormalizedWordsTokenizer(stopWords, minWordLength, maxWordLength)
 }
