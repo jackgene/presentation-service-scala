58703bb94be38be207b9d3cccfd8dc2ec3e02a22
Jack Leow <jackgene@gmail.com>
2023-10-28 10:55:29 -0700

    Footer text.

--- service-play/app/assets/javascripts/Deck/Deck/Slide/Template.elm
+++ service-play/app/assets/javascripts/Deck/Deck/Slide/Template.elm
@@ -5,7 +5,7 @@ import Css exposing
   ( borderTop3, bottom, display, height, left, margin, margin4
   , padding2, position, right, top, width
   -- Content
-  , backgroundColor, color, fontSize, fontWeight, verticalAlign
+  , color, fontSize, fontWeight, verticalAlign
   -- Units
   , em, vw, zero
   -- Alignments & Positions
@@ -59,7 +59,7 @@ standardSlideView page heading subheading content =
         ]
       ]
       [ div [ css [ display inlineBlock, position absolute, right zero ] ]
-        [ text "Kafka as the Source of Truth"
+        [ text "Functional Reactive Streaming with Kotlin Flow"
         , numberedDisc (toString page) 50
           [ css [ width (vw 2.5), margin4 zero zero (em 0.1) (em 0.4), verticalAlign middle ] ]
         ]
