0a150a5ec580a9fba0c3d53419d0e34307b2ebdf
Jack Leow <jackgene@gmail.com>
2023-11-04 14:41:31 -0700

    Improved source file names.

---
+++ service-play/app/actors/adapter/ActorFlowOps.scala
@@ -0,0 +1,57 @@
+package actors.adapter
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.actor.typed.scaladsl.adapter.*
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior, PostStop, Terminated}
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.stream.typed.scaladsl.ActorSource
+import akka.stream.{Materializer, OverflowStrategy}
+import org.reactivestreams.Publisher
+import play.api.libs.streams.ActorFlow
+
+/**
+ * Adapts Play Framework to typed Akka actors.
+ */
+implicit class ActorFlowOps(val ext: ActorFlow.type) extends AnyVal {
+  def sourceBehavior[Out](
+    behavior: ActorRef[Out] => Behavior[?],
+    bufferSize: Int = 16,
+    overflowStrategy: OverflowStrategy = OverflowStrategy.dropHead
+  )(implicit factory: ActorSystem, mat: Materializer): Source[Out, NotUsed] = {
+    val (
+      publisherActor: ActorRef[Option[Out]], publisher: Publisher[Option[Out]]
+    ) = ActorSource.
+      actorRef[Option[Out]](
+        completionMatcher = { case None => () },
+        failureMatcher = PartialFunction.empty,
+        bufferSize = bufferSize,
+        overflowStrategy = overflowStrategy
+      ).
+      toMat(Sink.asPublisher(false))(Keep.both).
+      run()
+    factory.spawnAnonymous(
+      Behaviors.setup { (ctx: ActorContext[Out]) =>
+        ctx.watch(publisherActor)
+        ctx.spawnAnonymous(behavior(ctx.self))
+
+        Behaviors.
+          receiveMessage { (out: Out) =>
+            publisherActor ! Some(out)
+            Behaviors.same[Out]
+          }.
+          receiveSignal {
+            case (_, Terminated(_)) => Behaviors.stopped
+            case (_, PostStop) =>
+              publisherActor ! None
+              Behaviors.same
+          }
+      }
+    )
+
+    Source.
+      fromPublisher(publisher).
+      collect { case Some(out) => out }
+  }
+}
