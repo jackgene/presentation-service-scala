09b51759c1c9e3c5ef3ed29cad2172985bd0124b
Jack Leow <jackgene@gmail.com>
2023-12-05 06:25:02 -0800

    service-akkahttp to use Scala 3 @main.

--- service-akkahttp/src/main/scala/com/jackleow/presentation/App.scala
+++ service-akkahttp/src/main/scala/com/jackleow/presentation/App.scala
@@ -37,6 +37,7 @@ class App(override val configuration: Configuration, htmlFile: File, port: Int)
     implicit system: ActorSystem[?]
   ): Unit =
     import system.executionContext
+    logger.debug(s"Starting presentation service with configuration\n$configuration")
 
     val bindingFut = Http()
       .newServerAt("0.0.0.0", port)
@@ -48,50 +49,3 @@ class App(override val configuration: Configuration, htmlFile: File, port: Int)
       case Failure(ex) =>
         logger.error("Failed to bind HTTP endpoint, terminating system", ex)
         system.terminate()
-
-object App extends StrictLogging:
-  private case class Arguments(
-    htmlFile: File = new File(""),
-    port: Int = 8973
-  )
-  private val builder = OParser.builder[Arguments]
-  private val parser =
-    import builder.*
-    OParser.sequence(
-      programName("presentation-service"),
-      head("Presentation Service in Akka HTTP"),
-      opt[File]("html-path")
-        .required()
-        .action:
-          (file: File, args: Arguments) =>
-            args.copy(htmlFile = file)
-        .text("Presentation HTML file path"),
-      opt[Int]('p', "port")
-        .action:
-          (port: Int, args: Arguments) =>
-            args.copy(port = port)
-        .text("HTTP server port"),
-    )
-
-  def main(rawArgs: Array[String]): Unit =
-    (
-      OParser.parse(parser, rawArgs, Arguments()),
-      ConfigSource.default.load[Configuration]
-    ) match
-      case (None, _) =>
-        System.exit(1)
-
-      case (Some(Arguments(htmlFile: File, _)), _)
-          if !htmlFile.exists() || htmlFile.isDirectory =>
-        Console.err.println(s"Deck file not found: ${htmlFile.getAbsolutePath}")
-        System.exit(1)
-
-      case (_, Left(failures: ConfigReaderFailures)) =>
-        Console.err.println(
-          s"Unable to load configuration:\n${failures.prettyPrint(1)}"
-        )
-        System.exit(1)
-
-      case (Some(args: Arguments), Right(configuration: Configuration)) =>
-        logger.debug(s"Starting presentation service with configuration\n$configuration")
-        val _ = new App(configuration, args.htmlFile, args.port)
