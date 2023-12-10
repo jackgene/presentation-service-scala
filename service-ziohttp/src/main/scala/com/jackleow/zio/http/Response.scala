6fd7249a31a1be1ca9e2f7bdbcfd51c8616d4f2a
Jack Leow <jackgene@gmail.com>
2023-12-10 02:28:06 -0800

    Minor extension method adjustment.

--- service-ziohttp/src/main/scala/com/jackleow/zio/http/Response.scala
+++ service-ziohttp/src/main/scala/com/jackleow/zio/http/Response.scala
@@ -3,4 +3,4 @@ package com.jackleow.zio.http
 import zio.http.{Response, Status}
 
 extension (response: Response.type)
-  def noContent: Response = Response.status(Status.NoContent)
+  def noContent: Response = response.status(Status.NoContent)
