db85547834b075be21fc7a735154f5a6ea83b065
Jack Leow <jackgene@gmail.com>
2023-12-13 07:08:01 -0800

    Added ziohttp basic chat message support.

--- service-ziohttp/src/main/scala/com/jackleow/presentation/PresentationApp.scala
+++ service-ziohttp/src/main/scala/com/jackleow/presentation/PresentationApp.scala
@@ -1,5 +1,7 @@
 package com.jackleow.presentation
 
+import com.jackleow.presentation.service.interactive.ChatMessageBroadcaster.ChatMessage
+import com.jackleow.presentation.service.interactive.InteractiveService
 import com.jackleow.presentation.service.transcription.TranscriptionBroadcaster
 import com.jackleow.zio.http.noContent
 import zio.*
@@ -11,13 +13,16 @@ import zio.json.EncoderOps
 import zio.stream.ZStream
 
 import java.nio.file
+import scala.util.matching.Regex
 
 object PresentationApp {
   private val contentTypeHtml: Headers = Headers(
     Header.ContentType(MediaType.text.html, charset = Option(Charsets.Utf8))
   )
+  private val RoutePattern: Regex = """(.*) to (Everyone|You)(?: \(Direct Message\))?""".r
+  private val IgnoredRoutePattern: Regex = "You to .*".r
 
-  def apply(htmlPath: file.Path): HttpApp[TranscriptionBroadcaster] =
+  def apply(htmlPath: file.Path): HttpApp[InteractiveService & TranscriptionBroadcaster] =
     Routes(
       // Deck
       Method.GET / empty -> handler:
@@ -53,6 +58,29 @@ object PresentationApp {
             ZStream.fromResource("html/moderator.html")
           )
         )
+      ,
+      Method.POST / "chat" -> handler: (req: Request) =>
+        (req.url.queryParams.get("route"), req.url.queryParams.get("text")) match
+          case (Some(route: String), Some(text: String)) =>
+            route match
+              case RoutePattern(sender, recipient) =>
+                ZIO.serviceWithZIO[InteractiveService]:
+                  (service: InteractiveService) =>
+                    service
+                      .receiveChatMessage(ChatMessage(sender, recipient, text))
+                      .tap: (succeeded: Boolean) =>
+                        ZIO.log(if succeeded then "SUCCESS" else "FAILED")
+                      .map(_ => Response.noContent)
+              case IgnoredRoutePattern() =>
+                ZIO.succeed(Response.noContent)
+              case _ =>
+                ZIO.succeed(Response.badRequest)
+          case (Some(_), None) =>
+            ZIO.succeed(Response.badRequest("Missing parameter: text"))
+          case (None, Some(_)) =>
+            ZIO.succeed(Response.badRequest("Missing parameter: route"))
+          case (None, None) =>
+            ZIO.succeed(Response.badRequest("Missing parameters: route, text"))
       ,
       Method.GET / "reset" -> handler:
         Response.noContent
